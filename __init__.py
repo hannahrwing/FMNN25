26cde25dcd09e76343da591c2d0fc3e1c34518e1
papinat0r <fredriksson.anton@yahoo.se>
2021-09-08 14:37:15 +0200

    stage work

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 13:18:53 2021
+
+@author: antonfredriksson
+"""
+
--- cubic_spline.py
+++ cubic_spline.py
@@ -15,16 +15,20 @@ class cubic_spline:
         self.control_points = control_points
         
     def __call__(self):
-        pass
+        uvec = linspace(self.grid[2], self.grid[-3], 1000)
+        sol = zeros((len(uvec), 2))
         
+        for i in range(len(uvec)):
+            sol[i,:] = self.blossom(uvec[i])
+    
+        return sol
     
     def blossom(self,u):
-        #check if in interval
         index = self.hot_interval(u)
         d = self.get_control_points(index)
         dA = self.interpolation(d[0], d[1], u, index-2, index+1)
         dB = self.interpolation(d[1], d[2], u, index-1, index+2)
-        dC = self.interpolation(d[2], d[3], u, index, index+2)
+        dC = self.interpolation(d[2], d[3], u, index, index+3)
         dAB = self.interpolation(dA, dB, u, index-1, index+1)
         dBC = self.interpolation(dB, dC, u, index, index+2)
         return self.interpolation(dAB, dBC, u, index, index+1)
@@ -43,11 +47,45 @@ class cubic_spline:
     #returns index
     def hot_interval(self,u):
         for i in range(len(self.grid)):
-            if (u < self.grid[i]):
+            if (u == self.grid[2]):
+                return 2
+            if (u <= self.grid[i]):
                 return i-1
+
             
     def get_control_points(self,index):
         return self.control_points[index-2:index+2]
+    
+    def plot(self, sol, deboor = False):
+        x = sol[:,0]
+        y = sol[:,1]
+        plot(x, y)
+        
+        if(deboor):
+            plot(self.control_points[:,0], self.control_points[:,1], linestyle = 'dashed' )
+            
+            
+        
+        
+control_points = array([[-1,-1],[2,-1],[3,-2],[3,1],[6,3],[1,2],[-1,2],[-2,0]])
+
+m = 10
+grid = zeros(m)
+grid[3:-3] = linspace(0,1,m-6)
+grid[0:3] = -0.1
+grid[-3:m] = 1.1
+
+spline = cubic_spline(grid, control_points)
+sol = spline()
+spline.plot(sol, True)
+        
+        
+#vectorize?
+#blossom recursive?
+#private help methods?
+#inheritance?
+#plot method parameters
+        
     
     
     
