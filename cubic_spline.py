07919cb4164a798e68810f5c6d899121ef5ed638
williamesamuelson <william.samuelson@gmail.com>
2021-09-10 08:42:37 +0200

    Stage work

--- cubic_spline.py
+++ cubic_spline.py
@@ -2,15 +2,16 @@
 """
 Created on Tue Sep  7 10:18:07 2021
 
-@author: willi
 """
 from numpy import *
 from matplotlib.pyplot import *
 
 
-#hejhej
-class cubic_spline:
+class CubicSpline:
     def __init__(self, grid, control_points):
+        if not len(grid) == len(control_points) + 2:
+            raise Exception("# of grid points are expected to be 2 more than # of control points")
+        #sort grid?    
         self.grid = grid
         self.control_points = control_points
         
@@ -24,60 +25,62 @@ class cubic_spline:
         return sol
     
     def blossom(self,u):
-        index = self.hot_interval(u)
+        index = self._hot_interval(u)
         d = self.get_control_points(index)
-        dA = self.interpolation(d[0], d[1], u, index-2, index+1)
-        dB = self.interpolation(d[1], d[2], u, index-1, index+2)
-        dC = self.interpolation(d[2], d[3], u, index, index+3)
-        dAB = self.interpolation(dA, dB, u, index-1, index+1)
-        dBC = self.interpolation(dB, dC, u, index, index+2)
-        return self.interpolation(dAB, dBC, u, index, index+1)
+        dA = self._interpolation(d[0], d[1], u, index-2, index+1)
+        dB = self._interpolation(d[1], d[2], u, index-1, index+2)
+        dC = self._interpolation(d[2], d[3], u, index, index+3)
+        dAB = self._interpolation(dA, dB, u, index-1, index+1)
+        dBC = self._interpolation(dB, dC, u, index, index+2)
+        return self._interpolation(dAB, dBC, u, index, index+1)
         
         
-    def interpolation(self,d1,d2,u,leftmost,rightmost):
-        alpha =  self.alpha(u,leftmost,rightmost)
+    def _interpolation(self,d1,d2,u,leftmost,rightmost):
+        alpha = self._alpha(u,leftmost,rightmost)
         
-        return alpha*d1 + (1-alpha)*d2
+        return alpha*array(d1) + (1-alpha)*array(d2)
         
-    def alpha(self,u,leftmost,rightmost):
+    def _alpha(self,u,leftmost,rightmost):
         if (leftmost == rightmost):
             return 0
         return (self.grid[rightmost] - u)/ (self.grid[rightmost]-self.grid[leftmost])
     
     #returns index
-    def hot_interval(self,u):
+    def _hot_interval(self,u):
         for i in range(len(self.grid)):
             if (u == self.grid[2]):
                 return 2
-            if (u <= self.grid[i]):
+            elif (u <= self.grid[i]):
                 return i-1
 
             
     def get_control_points(self,index):
         return self.control_points[index-2:index+2]
     
-    def plot(self, sol, deboor = False):
+    def plot(self, sol, de_Boor = False):
         x = sol[:,0]
         y = sol[:,1]
         plot(x, y)
         
-        if(deboor):
-            plot(self.control_points[:,0], self.control_points[:,1], linestyle = 'dashed' )
+        if(de_Boor):
+            de_Boor_x = [self.control_points[i][0] for i in range(len(self.control_points))]
+            de_Boor_y = [self.control_points[i][1] for i in range(len(self.control_points))]             
+            plot(de_Boor_x, de_Boor_y, linestyle = 'dashed' )
             
             
         
-        
-control_points = array([[-1,-1],[2,-1],[3,-2],[3,1],[6,3],[1,2],[-1,2],[-2,0]])
-
-m = 10
-grid = zeros(m)
-grid[3:-3] = linspace(0,1,m-6)
-grid[0:3] = -0.1
-grid[-3:m] = 1.1
-
-spline = cubic_spline(grid, control_points)
-sol = spline()
-spline.plot(sol, True)
+if __name__ =='__main__':       
+    control_points = array([[-1,5],[2,-1],[3,-2],[3,1],[6,3],[1,2],[-1,2],[-2,0]])
+    
+    m = 10
+    grid = zeros(m)
+    grid[3:-3] = linspace(0,1,m-6)
+    grid[0:3] = -0.1
+    grid[-3:m] = 1.1
+    
+    spline = CubicSpline(grid, control_points)
+    sol = spline()
+    spline.plot(sol, True)
         
         
 #vectorize?
