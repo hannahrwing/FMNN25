49650bf11789e6a1359686bf72c1d0afee46c999
Hanna Hörwing <hannahrwing@gmail.com>
2021-09-14 13:12:54 +0200

    add comments

--- cubic_spline.py
+++ cubic_spline.py
@@ -5,13 +5,9 @@ Created on Tue Sep  7 10:18:07 2021
 """
 import unittest
 from numpy import *
-from matplotlib import pyplot as plt
-import numpy as np
 from matplotlib.pyplot import *
 from test_points_2 import control_points_2, grid_2
 import os
-
-
 
 class CubicSpline:
     def __init__(self, grid, control_points):
@@ -25,13 +21,18 @@ class CubicSpline:
     def __call__(self):
         uvec = linspace(self.grid[2], self.grid[-3], 1000)
         sol = array([list(self.blossom(u)) for u in uvec])
-    
         return sol
     
+    
     def blossom(self,u,index_given = False, index = 0):
+        """
+        Runs the blossom recursion to obtain s(u). 
+        Can also be used to return the blossoms 
+        for the add-on assignment. 
+        """
         if(not index_given):
             index = self._hot_interval(u)
-        d = self.get_control_points(index)
+        d = self._get_control_points(index)
         dA = self._interpolation(d[0], d[1], u, index-2, index+1) 
         dB = self._interpolation(d[1], d[2], u, index-1, index+2)
         dC = self._interpolation(d[2], d[3], u, index, index+3)
@@ -44,17 +45,21 @@ class CubicSpline:
         
         
     def _interpolation(self,d1,d2,u,leftmost,rightmost):
-        alpha = self._alpha(u,leftmost,rightmost)
+        """
+        Determines the scalar factor alpha(u) and 
+        thereafter uses linear interpolation. 
+        """
+        if (leftmost == rightmost):
+            alpha = 0;
+        alpha = (self.grid[rightmost] - u)/ (self.grid[rightmost]-self.grid[leftmost])
         
         return alpha*array(d1) + (1-alpha)*array(d2)
-        
-    def _alpha(self,u,leftmost,rightmost):
-        if (leftmost == rightmost):
-            return 0
-        return (self.grid[rightmost] - u)/ (self.grid[rightmost]-self.grid[leftmost])
-    
-    #returns index
+
+
     def _hot_interval(self,u):
+        """
+        Finds the "hot" interval.
+        """
         for i in range(len(self.grid)):
             if (u == self.grid[2]):
                 return 2
@@ -62,10 +67,19 @@ class CubicSpline:
                 return i-1
 
             
-    def get_control_points(self,index):
+    def _get_control_points(self,index):
+        """
+        Selects the four control points 
+        corresponding to the hot interval.
+        """
         return self.control_points[index-2:index+2]
     
+    
     def plot(self, sol, de_Boor = False, addon_blossoms = False, addon_index = 0):
+        """
+        Plots the solution along with 
+        (optional) De Boor control points and (optional) blossoms. 
+        """
         x = sol[:,0]
         y = sol[:,1]
         plot(x, y)
@@ -79,7 +93,6 @@ class CubicSpline:
             
             dAB_x = [d_point[0, 0] for d_point in d_points]
             dAB_y = [d_point[0, 1] for d_point in d_points]
-           
             dA_x = [d_point[1, 0] for d_point in d_points]
             dA_y = [d_point[1, 1] for d_point in d_points]
             
@@ -95,57 +108,67 @@ class CubicSpline:
             leg.append("Control points")
         
         legend(leg)
-        plt.show()
-        plt.close()
+        show()
+        close()
         
         
 def basis_function(i, grid, k = 3):
-    if k==3:
+    """
+    Returns the i:th basis function. 
+    """
+    if k == 3:
         grid = insert(grid, 0, 0)
         grid = append(grid, grid[-1]+1)
         i = i+1 #Because of the padded grid
     
     def base_case_function(u):
+        """
+        Returns value of basis function for k == 0. 
+        """
         return 0 if (grid[i-1] == grid[i]) else (1 if grid[i-1] <= u < grid[i] else 0)
     
     if k == 0:
         return base_case_function
     else:
         def iteration_function(u):
-            if grid[i+k-1] == grid[i-1]: #hmmm
+            """
+            Returns value of basis function for k != 0.
+            """
+            if grid[i+k-1] == grid[i-1]: #if nodes coincide
                 factor_1 = 0
             else:
                 factor_1 = (u - grid[i-1]) / (grid[i+k-1] - grid[i-1])
                 
-            if grid[i+k] == grid[i]:
+            if grid[i+k] == grid[i]: #if nodes coincide
                 factor_2 = 0
             else:
                 factor_2 = (grid[i+k] - u ) / (grid[i+k] - grid[i])
+                
             return factor_1 * basis_function(i, grid, k-1)(u) + factor_2 * basis_function(i+1, grid, k-1)(u)
+        
         return iteration_function
         
 
 if __name__ =='__main__':       
-    #control_points = array([[-1,5],[2,-1],[3,-2],[3,1],[6,3],[1,2],[-1,2],[-2,0]])
     control_points = control_points_2
     grid = grid_2
+    
     spline = CubicSpline(grid, control_points)
     basis_functions = [basis_function(i,grid) for i in range(5,20)]
-    N1 = basis_function(5,grid,k=3)
     sol = spline()
+    
     x = linspace(0, 1, 1000)
     y = [[basis_functions[i](u) for u in x] for i in range(len(basis_functions))]
-    figure()
+    
+    figure(dpi = 600)
+    title("Basis functions")
     for i in range(len(y)):
-        plt.plot(x,y[i])
-    figure()
-    spline.plot(sol, de_Boor=True, addon_blossoms=True, addon_index=5)
-    os.system("python -m unittest discover")
-    plt.show(block=False)
-#vectorize?
-#inheritance?
-#plot method parameters
+        plot(x,y[i])
         
+    figure(dpi = 600)
+    title("B-spline solution")
+    spline.plot(sol, de_Boor=True, addon_blossoms=True, addon_index=2)
     
-    
+    os.system("python -m unittest discover")
+    show(block=False)
     
--- tests.py
+++ tests.py
@@ -5,22 +5,30 @@ from test_points_2 import control_points_2, grid_2
 class Test(unittest.TestCase):
     
     def setUp(self):
+        """
+        Create spline object and basis functions.
+        """
         self.spline = CubicSpline(grid_2, control_points_2)
         self.basis_functions = [basis_function(i,grid_2) for i in range(len(self.spline.control_points))]
     
     def test_unity(self):
+        """
+        Test if basis functions add up to 1. 
+        """
         basis_functions = self.basis_functions
         expected = 1
         
         u_vec = linspace(0,1)
         for u in u_vec:
             result = 0
-            #hot interval?
             for N in basis_functions:
                 result += N(u)
             self.assertAlmostEqual(result, expected)
             
     def test_positive(self):
+        """
+        Test if basis functions positive. 
+        """
         spline = self.spline
         basis_functions = self.basis_functions
         
@@ -30,6 +38,9 @@ class Test(unittest.TestCase):
                 self.assertGreaterEqual(N(u), 0)
         
     def test_evaluate(self):
+        """
+        Test if De Boor algorithm and the basis functions generate same results. 
+        """
         basis_functions = self.basis_functions
         spline = self.spline
         u_vec = linspace(spline.grid[2],spline.grid[-3])
@@ -43,6 +54,9 @@ class Test(unittest.TestCase):
             self.assertAlmostEqual(sum[1], spline.blossom(u)[1])
             
     def test_evaluate_point(self):
+        """
+        Test if test point accurate. 
+        """
         spline = self.spline
         u = 0.2
         expected = array([-31.90219167, 6.47655833])
