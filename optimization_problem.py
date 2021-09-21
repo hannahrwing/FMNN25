acbff6da57ca7cc52d029a9062198d19e2bf719b
NilRom <nils.romanus@gmail.com>
2021-09-21 16:07:29 +0200

    added more methods

--- optimization_problem.py
+++ optimization_problem.py
@@ -2,7 +2,7 @@
 
 from numpy import *
 from matplotlib.pyplot import *
-from optimization_method import ClassicNewton, BFGS
+from optimization_method import ClassicNewton, BFGS, DFP, GoodBroyden, SymmetricBroyden
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     # problem.plot(interval,steps)
     interval = [linspace(-0.7, 2, num=1000), linspace(-1.5, 4, num = 1000)]
     problem = OptimizationProblem(f, grad)
-    method = BFGS(False)
+    method = DFP(False)
     x0 = [0.5,3]
     sol, steps = method(problem, x0)
-    print(steps)
     problem.plot(interval,steps)
+    print(F"Minimum at: {sol}")
