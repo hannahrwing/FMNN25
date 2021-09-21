279ed36d46bb0f382fba3a694171ed6d224f90b0
NilRom <nils.romanus@gmail.com>
2021-09-21 13:00:57 +0200

    added plot thing

---
+++ optimization_method.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+from numpy import *
+from matplotlib.pyplot import *
+from scipy.optimize import minimize
+
+class OptimizationMethod:
+        
+    def __init__(self, exact_line_search = True):
+        self.exact_line_search = exact_line_search
+        
+        
+    def __call__(self, problem, x0):
+        H = self.hessian(x0, problem.func)
+        x,H = self.step(H, x0, problem)
+        x_old = x0
+        tol = 1e-5
+        steps = [x_old]
+        while linalg.norm(problem.gradient(x)) > tol and linalg.norm(x-x_old) > tol:
+            x_old = x
+            x, H = self.step(H, x, problem)
+            steps = np.vstack((steps, x_old))
+    
+        return x, steps
+    def step(self):
+        raise NotImplementedError()
+        
+
+    
+    
+class Newton(OptimizationMethod):
+    
+    
+    
+    def step(self, H, x, problem):
+        if problem.gradient == None:
+            #calculate it
+            pass
+        else:
+            s = -H @ problem.gradient(x)
+        if self.exact_line_search:
+            alpha = self.exact_search(x, s, problem.func)
+        else:
+            alpha = self.inexact_search(x, s, problem.func)
+        x_new = x + alpha*s
+        H_new = self.hessian(x, problem.func, H)
+        return x_new, H_new
+    
+    def hessian(self):
+        raise NotImplementedError()
+        
+    def exact_search(self, x, s, f):
+        def func(alpha):
+            return(f(x+alpha*s))
+        return minimize(func, 0).x
+    
+    def inexact_search(self, x, s, f):
+        pass
+
+
+class ClassicNewton(Newton):
+    
+    
+    def hessian(self, x, f, H_prev = None):
+        n = len(x)
+        G = zeros((n,n))
+        h = 1e-3
+        
+        for i in range(n):
+            for j in range(n):
+                G[i,j] = (f(x + h*self._basisvec(n,(i,j),(1,1))) - f(x + h*self._basisvec(n,(i,j), (1,-1)))
+                          - f(x + h*self._basisvec(n,(i,j),(-1,1))) + f(x + h*self._basisvec(n,(i,j),(-1,-1))))/(4*h**2)
+        G = (G + G.T)/2
+        return linalg.inv(G)
+    
+    def _basisvec(self, n, i, val):
+        v = zeros(n)
+        v[i[0]] += val[0]
+        v[i[1]] += val[1]
+        return v
---
+++ optimization_problem.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from numpy import *
+from matplotlib.pyplot import *
+from optimization_method import ClassicNewton
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+class OptimizationProblem:
+    
+    def __init__(self, func, gradient = None):
+        self.func = func
+        self.gradient = gradient
+    
+    def plot(self, interval, steps):
+        
+        mpl.rcParams['figure.dpi'] = 300
+        fig, ax = plt.subplots(1,1)
+        X, Y = np.meshgrid(interval[0], interval[1])
+        X, Y
+        Z = np.array(100*(Y-X**2)**2 + (1-X)**2)
+        
+        N = 24
+        big = linspace(20.1,1250, num = 10)
+        small = linspace(0,3, num = 7)
+        medium = linspace(3.1,20, num = 10)
+        levels = np.concatenate((small, big), axis = 0)
+    
+        cp = ax.contour(X, Y, 100*(Y-X**2)**2 + (1-X)**2, levels=levels,
+                        colors = 'black')
+        plt.clabel(cp, inline=1, fontsize=10)
+        plt.setp(cp.collections , linewidth=0.75)
+        plt.plot(steps[0], steps[1], linestyle = 'dashed', marker = 'o',
+                 markersize = 2, color = 'black')
+        plt.xlim((interval[0][0], interval[0][len(interval[0])-1]))
+        plt.ylim((interval[1][0], interval[1][len(interval[1])-1]))
+        plt.show()
+    
+    
+    
+def f(x):
+    return 100*(x[1]-x[0]**2)**2 + (1-x[0])**2
+    
+def grad(x):
+    return [-400*(x[1]-x[0]**2)*x[0] - 2*(1-x[0]), 200*(x[1]-x[0]**2)]    
+    
+if __name__ == '__main__':
+    
+    problem = OptimizationProblem(f, grad)
+    method = ClassicNewton()
+    x0 = [0,-0.8]
+    sol, steps = method(problem, x0)
+    print(sol)
+    steps = np.transpose((np.array(steps)))
+    interval = [linspace(-0.7, 2, num=1000), linspace(-1.5, 4, num = 1000)]
+    problem.plot(interval,steps)
