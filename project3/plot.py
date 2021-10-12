f15be9d4b8fcf8b17314a6a284e6108c4cc93a88
NilRom <nils.romanus@gmail.com>
2021-10-12 10:42:36 +0200

    hi

--- project3/plot.py
+++ project3/plot.py
@@ -29,29 +29,20 @@ class Plotter():
         Nx = nx1 + nx2 + nx3
         Ny = ny2
         big = np.zeros((Ny, Nx))
-        print(np.shape(big))
         
         big[0:0+self.sols[0].shape[0], 0:0+self.sols[0].shape[1]] += self.sols[0]
         big[0:0 + self.sols[1].shape[0], nx1 : nx1+self.sols[1].shape[1]] += self.sols[1]
         big[nx1:nx1 + self.sols[2].shape[0], nx1+nx2 : nx1+nx2+self.sols[2].shape[1]] += self.sols[2]
         
         
-        print(big)
-        #plt.imshow(big)
-        #plt.show()  
         X, Y = np.meshgrid(np.linspace(0,1,big.shape[1]), np.linspace(0,1,big.shape[0]))
         big[big == 0.0] = np.NaN
-        #zzs[zzs < 0.] = np.NaN
-        #print(big)
         fig = plt.figure(dpi=600)
-        #ax = fig.add_subplot(111, projection='3d')
+
         ax = fig.add_subplot(111)
         
         
-        #surf = ax.plot_surface(X, Y, big,  cmap=cm.coolwarm, linewidth=0, antialiased=False, vmin = 5, vmax = 35)
+       
         cp = ax.contourf(X,Y, big)
-
-        #ax.zaxis.set_major_locator(LinearLocator(10))
-        #ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
         
         plt.show()
